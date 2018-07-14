52e8bf2f880565a09ee5d63bfe23b6601e79e355
Master Keits <master@furiosa.lan>
2018-07-14 17:37:14 +0300

    themeV11

--- myaddons/web_responsive/__manifest__.py
+++ myaddons/web_responsive/__manifest__.py
@@ -1,15 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 LasLabs Inc.
+# Copyright 2018 Alexandre Díaz
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Web Responsive",
     "summary": "It provides a mobile compliant interface for Odoo Community "
                "web",
-    "version": "10.0.1.2.3",
+    "version": "11.0.1.0.2",
     "category": "Website",
     "website": "https://laslabs.com/",
-    "author": "LasLabs, Tecnativa, Odoo Community Association (OCA)",
+    "author": "LasLabs, Tecnativa, Alexandre Díaz, "
+              "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
     "depends": [
@@ -18,9 +19,9 @@
     "data": [
         'views/assets.xml',
         'views/web.xml',
+        'views/inherited_view_users_form_simple_modif.xml',
     ],
     'qweb': [
-        'static/src/xml/app_drawer_menu_search.xml',
         'static/src/xml/form_view.xml',
         'static/src/xml/navbar.xml',
     ],
